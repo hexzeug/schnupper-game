8e0e79bc367b5eafa6743980c3c4a81b847d6b3b
Johann <95136426+Gunnify@users.noreply.github.com>
2023-10-19 21:13:33 +0200

    obstacle update direct x,y

--- model/obstacle.py
+++ model/obstacle.py
@@ -20,12 +20,17 @@ class Obstacle(object):
     def draw(self):
         self.actor.draw()
     
-    def update(self):
-        if self.actor.x < -self.actor.width:  # Reset the position if the obstacle has gone of screen.
-            # The x position is set to a random value, to prevent the obstacles from sticking together.
-            self.actor.x = random.randrange(
-                WIDTH + self.actor.width,
-                WIDTH + self.actor.width + 400
-            )  
-        else:  # If the obstacle is still on the screen move it to the left to create the feeling of running.
-            self.actor.x -= self.game.get_running_speed() - self.v[0]
+    def update(self, x = None, y = None):
+        if x is None and y is None:
+            if self.actor.x < -self.actor.width:  # Reset the position if the obstacle has gone of screen.
+                # The x position is set to a random value, to prevent the obstacles from sticking together.
+                self.actor.x = random.randrange(
+                    WIDTH + self.actor.width,
+                    WIDTH + self.actor.width + 400
+                )  
+            else:  # If the obstacle is still on the screen move it to the left to create the feeling of running.
+                self.actor.x -= self.game.get_running_speed() - self.v[0]
+        
+        else:
+            self.actor.x = x
+            self.actor.y = y
